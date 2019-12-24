6db672095bff18e62346de4c4ad31712a4dcb97a
lc00734 <964758948@qq.com>
2019-12-24 12:13:55 +0800

    Initial commit

---
+++ src/main/java/com/realme/SpringbootTestAllApplication.java
@@ -0,0 +1,14 @@
+package com.realme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestAllApplication.class, args);
+        System.out.println("启动成功");
+    }
+
+}
