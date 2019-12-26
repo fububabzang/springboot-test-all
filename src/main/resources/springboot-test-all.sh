e4be545db0b92ae7ae2adba8acdb69df529c1a6e
lc00734 <964758948@qq.com>
2019-12-26 12:10:35 +0800

    12

--- src/main/resources/springboot-test-all.sh
+++ src/main/resources/springboot-test-all.sh
@@ -7,7 +7,7 @@ echo '----rm container----'
 docker run -p 8096:8096 --name ${app_name} \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
--d mall-tiny/${app_name}:1.0-SNAPSHOT
+-d mall-tiny/${app_name}:0.0.1-SNAPSHOT
 echo '----start container----'
 docker rmi `docker images | grep  "<none>" | awk '{print $3}'`
 echo 'delete all <none> image'
